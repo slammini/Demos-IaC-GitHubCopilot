568843254d1d5de2b9df491444952c94e11c646d
Sara Lammini <saralam@microsoft.com>
2023-07-03 16:29:10 +0200

    Trying out the storage account

--- Code/vm.tf
+++ Code/vm.tf
@@ -45,11 +45,12 @@ resource "azurerm_virtual_machine" "vm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
 
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
 
@@ -57,16 +58,19 @@ resource "azurerm_virtual_machine" "vm" {
     name              = "osdisk-terraform-azure-sisnet"
     caching           = "ReadWrite"
     create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
+    managed_disk_type = "Standard_LRS"
   }
 
   os_profile {
     computer_name  = "vm-terraform-azure-sisnet"
     admin_username = "adminuser"
-    admin_password = "Password1234!"
   }
 
   os_profile_linux_config {
-    disable_password_authentication = false
+    disable_password_authentication = true
+    ssh_keys {
+      path     = "/home/adminuser/.ssh/authorized_keys"
+      key_data = file("~/.ssh/id_rsa.pub")
+    }
   }
 }
