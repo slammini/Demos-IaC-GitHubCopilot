d99ded321c6a4383f4874d8314e055bfba2b3032
Sara Lammini <saralam@microsoft.com>
2023-07-03 17:51:33 +0200

    Deploying a new VM into Azure thanks to GitHub Copilot

--- Code/vm.tf
+++ Code/vm.tf
@@ -1,57 +1,58 @@
 # Create a new Azure virtual machine with password authentication
+# Create a new resource group, a new virtual network, a new subnet, a new public IP address, a new network interface, and a new virtual machine with basic configuration
 
 # Provider
 provider "azurerm" {
   features {}
 }
 
-# Resource
+# Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "Test-CC-TerraformVM"
+  name     = "Test-vm-TF"
   location = "westeurope"
 }
 
-# Create a virtual network
+# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "Test-CC-TerraformVM-vnet"
+  name                = "Test-vm-TF-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-# Create a subnet
+# Subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "Test-CC-TerraformVM-subnet"
+  name                 = "Test-vm-TF-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/24"]
 }
 
-# Create a public IP address
+# Public IP
 resource "azurerm_public_ip" "publicip" {
-  name                = "Test-CC-TerraformVM-publicip"
+  name                = "Test-vm-TF-publicip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
 }
 
-# Create newtork interface
+# Network Interface
 resource "azurerm_network_interface" "nic" {
-  name                = "Test-CC-TerraformVM-nic"
+  name                = "Test-vm-TF-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "Test-CC-TerraformVM-nic-ipconfig"
+    name                          = "Test-vm-TF-nic-ipconfig"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.publicip.id
   }
 }
 
-# Create a virtual machine with password authentication and basic configuration
+# Virtual Machine with password authentication and basic configuration
 resource "azurerm_virtual_machine" "vm" {
-  name                  = "Test-CC-TerraformVM"
+  name                  = "Test-vm-TF-vm"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
@@ -60,28 +61,25 @@ resource "azurerm_virtual_machine" "vm" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
   storage_os_disk {
-    name              = "Test-CC-TerraformVM-osdisk"
+    name              = "Test-vm-TF-osdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
 
   os_profile {
-    computer_name  = "Test-CC-TerraformVM"
-    admin_username = "azureuser"
+    computer_name  = "Test-vm-TF-vm"
+    admin_username = "testadmin"
     admin_password = "Password1234!"
   }
 
   os_profile_linux_config {
     disable_password_authentication = false
   }
-
-  tags = {
-    environment = "Terraform Demo"
-  }
 }
+
